c96830e54672295c740bb7fffdd3ba8cd26a2243
fali <fali@nisum.com>
2023-05-28 15:30:32 +0500

    Created task tracking

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,33 @@
+import { Task } from './Task';
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Doctors Appointment',
+    day: 'May 5th at 2:30pm',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Meeting at School',
+    day: 'May 6th at 1:30pm',
+    reminder: true,
+  },
+  {
+    id: 4,
+    text: 'Food test',
+    day: 'May 7th at 12:30pm',
+    reminder: false,
+  },
+  {
+    id: 5,
+    text: 'Food test test',
+    day: 'May 7th at 12:30pm',
+    reminder: false,
+  },
+  {
+    id: 6,
+    text: 'Food shopper',
+    day: 'May 7th at 12:30pm',
+    reminder: false,
+  },
+];
